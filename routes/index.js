31a3bff7be7071be1bc4cc2be708c842692aa918
David Castillo <david@castillog.com>
2022-01-19 21:20:10 -0400

    Index random and routes

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,33 @@
 const router = require("express").Router();
 const User = require("../models/User.model");
+const Recipe = require("../models/Recipe.model");
+const Review = require("../models/Review.model");
 const {isLoggedOut} = require("../utils/auth")
 
-// GET home page verify if user is logged in
+// Index route with 12 random recipes to display on the home page
 router.get("/", async (req, res, next) => {
     try {
       // If user isn't logged in, render index page
       if (!req.session.user) {
-        res.render("index");
+        const recipes = await Recipe.find({}).populate("_user");
+        const randomRecipes = [];
+        for (let i = 0; i < 12; i++) {
+            const randomRecipe = recipes[Math.floor(Math.random() * recipes.length)];
+            randomRecipes.push(randomRecipe);
+        }
+        const reviews = await Review.find({});
+        res.render("index", { recipes: randomRecipes, reviews });
       } else {
+        const recipes = await Recipe.find({}).populate("_user");
+        const randomRecipes = [];
+        for (let i = 0; i < 12; i++) {
+            const randomRecipe = recipes[Math.floor(Math.random() * recipes.length)];
+            randomRecipes.push(randomRecipe);
+        }
+        const reviews = await Review.find({});
         const user = await User.findById(req.session.user._id);
-        res.render("index", { user });
-      }
+        res.render("index", { recipes: randomRecipes, reviews, user });
+    }
     } catch (error) {
       next(error);
     }
